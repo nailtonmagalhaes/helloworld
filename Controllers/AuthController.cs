81202ba37ffc308b91a6ef971998a6d789947f2e
nailtonmagalhaes <nailton85@gmail.com>
2025-08-03 14:53:30 -0300

    Adicionando o yml

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,6 +1,7 @@
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using Models;
@@ -20,6 +21,7 @@ public class AuthController : ControllerBase
     }
 
     [HttpPost("login")]
+    [AllowAnonymous]
     public IActionResult Login([FromBody] LoginModel model)
     {
         if (model.Username != "admin" || model.Password != "password")
